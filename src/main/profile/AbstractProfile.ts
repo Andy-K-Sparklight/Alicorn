780a876ef54aeddfb85b6b42cf1a10d9f1f5e1c4
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2021-03-17 00:02:24 +0800

    feat(profile): wip abstract profile

--- src/main/profile/AbstractProfile.ts
+++ src/main/profile/AbstractProfile.ts
@@ -1,1 +1,12 @@
-export class AbstractProfile {}
+import {LibraryMeta, OptionalArgument} from "./Meta";
+
+export class AbstractProfile {
+    gameArgs: string[] = []
+    jvmArgs: OptionalArgument[] = []
+    id = ""
+    baseVersion = ""
+    libraries: LibraryMeta[] = []
+    
+}
+
+// TODO WIP
