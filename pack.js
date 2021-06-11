f7099740d6c926ff6b749a9af923c9b20c16adff
RarityEG <Twilight_Princess_Sparkle@outlook.com>
2021-06-11 23:28:49 +0800

    ci(*): use js to compress

---
+++ pack.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const compressing = require("compressing");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require("fs-extra");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+const tDir = "./out/";
+
+const all = fs.readdirSync(tDir);
+Promise.allSettled(
+  all.map((e) => {
+    const c = path.join(tDir, e);
+    const s = fs.statSync(c);
+    if (s.isDirectory()) {
+      if (e.startsWith("Alicorn-win32")) {
+        return compressing.zip.compressDir(c, path.join(tDir, e + ".zip"), {});
+      }
+      if (e.startsWith("Alicorn-linux")) {
+        return compressing.tgz.compressDir(c, path.join(tDir, e + ".tar.gz"));
+      }
+    }
+    return Promise.resolve();
+  })
+)
+  .then(() => {
+    console.log("Compressed output.");
+  })
+  .catch((e) => {
+    console.log("Could not compress output. Caused by: " + e);
+  });
