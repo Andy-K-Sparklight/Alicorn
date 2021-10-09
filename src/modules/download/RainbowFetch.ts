a4ace5a49f9b304b536f35ba0a3c39001152a132
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2021-10-09 17:01:41 +0800

    fix(download): fix forge web jar not found
    Fixed #47

--- src/modules/download/RainbowFetch.ts
+++ src/modules/download/RainbowFetch.ts
@@ -1,5 +1,6 @@
 import fs, { WriteStream } from "fs-extra";
 import { IntervalChecker, WatchDog } from "../commons/WatchDog";
+import { getNumber } from "../config/ConfigSupport";
 export function guardPipeFile(
   origin: NodeJS.ReadableStream,
   target: WriteStream,
@@ -106,4 +107,20 @@ export function getTimeoutController(
       }
     },
   ];
+}
+
+export async function isWebFileExist(u: string): Promise<string | false> {
+  try {
+    const [controller, sti] = getTimeoutController(
+      getNumber("download.concurrent.timeout", 5000)
+    );
+    const r = await fetch(u, { signal: controller.signal });
+    sti();
+    if (r.ok) {
+      return u;
+    }
+    return false;
+  } catch {
+    return false;
+  }
 }
