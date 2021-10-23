eb04810817ae1082e6a848a93dd7fe8a590a2b77
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2021-10-23 21:38:30 +0800

    chore(pff): remove unused code

--- src/modules/pff/curseforge/Values.ts
+++ src/modules/pff/curseforge/Values.ts
@@ -1,8 +1,2 @@
-import EventEmitter from "events";
-
 export const GAME_ID = "432";
-export const GLOBAL_CACHE_NAME = "pff";
-export const GLOBAL_FILE_NAME = "mod-files";
-export const CF_API_BASE_URL = "https://addons-ecs.forgesvc.net";
-export const NULL_OUTPUT = new EventEmitter();
 export const PFF_MSG_GATE = "message";
