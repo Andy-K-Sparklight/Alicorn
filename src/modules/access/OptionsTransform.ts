09d84cde6d9df834130b13d31c4825e4d1c87f91
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2021-12-31 18:20:22 +0800

    #78 feat(access): write basic options parser

---
+++ src/modules/access/OptionsTransform.ts
@@ -0,0 +1,38 @@
+import { readFile, writeFile } from "fs-extra";
+import { MinecraftContainer } from "../container/MinecraftContainer";
+
+const OPTIONS_FILE = "options.txt";
+
+export async function parseOptions(
+  c: MinecraftContainer
+): Promise<Map<string, string>> {
+  const f = c.resolvePath(OPTIONS_FILE);
+  const d = (await readFile(f)).toString().split("\n");
+  const out = new Map<string, string>();
+  d.forEach((v) => {
+    v = v.trim();
+    if (v.length === 0 || !v.includes(":")) {
+      return;
+    }
+    const [k, ...p] = v.split(":");
+    const cv = p.join(":");
+    out.set(k, cv);
+  });
+  return out;
+}
+
+// Warning: this will override!
+export async function buildOptions(
+  origin: Map<string, string>,
+  container: MinecraftContainer
+): Promise<void> {
+  const f = container.resolvePath(OPTIONS_FILE);
+  const o = [];
+  for (const [k, v] of origin.entries()) {
+    if (k.length > 0) {
+      const c = k + ":" + v; // Value may be empty
+      o.push(c);
+    }
+  }
+  await writeFile(f, o.join("\n"));
+}
