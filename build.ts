af45c24a7ac510c779b9d04baeab8b6292da3491
skjsjhb <skjsjhb@outlook.com>
2025-01-01 19:55:31 +0800

    feat(*): run instrumented tests on all supported platforms
    - Add xvfb-maybe to run app on headless runners.
    - Enable matrix to run on all platforms.

--- build.ts
+++ build.ts
@@ -22,9 +22,10 @@ await build({
 if (mode === "test") {
     consola.start("Running instrumented tests...");
 
+    const xvfbExec = path.resolve(import.meta.dirname, "node_modules", "xvfb-maybe", "src", "xvfb-maybe.js");
     const electronExec = path.resolve(import.meta.dirname, "node_modules", "electron", "cli.js");
     const cwd = path.resolve(import.meta.dirname, "build", "prod");
-    const proc = child_process.fork(electronExec, ["."], { cwd });
+    const proc = child_process.fork(xvfbExec, [electronExec, "."], { cwd });
 
     proc.once("exit", () => {
         const f = path.join(cwd, "test-summary.json");
