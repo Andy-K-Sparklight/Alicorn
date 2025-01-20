c8fcf145173306acb83b3f2e87dda7dc3978487b
skjsjhb <skjsjhb@outlook.com>
2025-01-20 23:08:06 +0800

    perf(hash): simplify code for hashing

--- src/main/security/hash-worker.ts
+++ src/main/security/hash-worker.ts
@@ -1,6 +1,7 @@
 import { createHash } from "node:crypto";
 import fs from "node:fs";
 import { parentPort, workerData } from "node:worker_threads";
+import { pEvent } from "p-event";
 
 export interface HashWorkerData {
     path: string;
@@ -8,28 +9,26 @@ export interface HashWorkerData {
     expect?: string;
 }
 
-function main() {
+async function main() {
     const { path, algorithm, expect } = workerData as HashWorkerData;
 
     const hash = createHash(algorithm);
     const rs = fs.createReadStream(path);
 
     rs.on("data", (chunk) => hash.update(chunk));
-    rs.once("end", () => {
+
+    let result: unknown;
+
+    try {
+        await pEvent(rs, "end");
+
         const h = hash.digest("hex").toLowerCase().trim();
-        if (expect) {
-            parentPort?.postMessage(h === expect.toLowerCase().trim());
-        } else {
-            parentPort?.postMessage(h);
-        }
-    });
-    rs.once("error", () => {
-        if (expect) {
-            parentPort?.postMessage(false);
-        } else {
-            parentPort?.postMessage("");
-        }
-    });
+        result = expect ? h === expect.toLowerCase().trim() : h;
+    } catch {
+        result = expect ? false : "";
+    }
+
+    parentPort!.postMessage(result);
 }
 
 void main();
