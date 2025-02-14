00cd1a9caaf7cc04afcf40215b307bea28ca4523
skjsjhb <skjsjhb@outlook.com>
2025-02-14 16:46:48 +0800

    chore(build): add build script for win32 installers

--- pack.ts
+++ pack.ts
@@ -5,6 +5,7 @@ import { type Options, packager } from "@electron/packager";
 import { tgz, zip } from "compressing";
 import consola from "consola";
 import { createDMG } from "electron-installer-dmg";
+import { MSICreator } from "electron-wix-msi";
 import fs from "fs-extra";
 import os from "node:os";
 import path from "node:path";
@@ -51,16 +52,18 @@ for (const platform of platforms) {
         if (platform === "win32") {
             consola.start(`Creating zip archive from ${outPath}`);
             await zip.compressDir(outPath, outPath + ".zip", { ignoreBase: true });
+            consola.success(`Archive written to ${outPath + ".zip"}`);
+
+            if (os.platform() === "win32") {
+                await buildWindowsInstaller(outPath, arch);
+            } else {
+                consola.log("MSI images can only be created on Windows, skipped.");
+            }
         }
 
         if (platform === "darwin") {
             if (os.platform() === "darwin") {
-                consola.start(`Creating DMG image from ${outPath}`);
-                await createDMG({
-                    appPath: path.join(outPath, "Alicorn Launcher.app"),
-                    name: "Alicorn Launcher",
-                    out: outPath + ".dmg"
-                });
+                await buildDMG(outPath);
             } else {
                 consola.log("DMG images can only be created on macOS, skipped.");
             }
@@ -74,3 +77,50 @@ for (const platform of platforms) {
 }
 
 consola.success("done.");
+
+async function buildWindowsInstaller(outPath: string, arch: string) {
+    consola.start(`Creating MSI installers from ${outPath}`);
+
+    const root = path.dirname(outPath);
+
+    const msiCreator = new MSICreator({
+        appDirectory: outPath,
+        description: pkg.description,
+        exe: "Alicorn.exe",
+        name: "Alicorn Launcher",
+        shortName: "Alicorn",
+        manufacturer: "Alicorn Launcher",
+        version: pkg.version,
+        // @ts-expect-error The type is not updated
+        arch: arch,
+        features: {
+            autoLaunch: false,
+            autoUpdate: false
+        },
+        ui: {
+            chooseDirectory: true
+        },
+        icon: path.resolve(import.meta.dirname, "resources/icons/icon.ico"),
+        outputDirectory: root
+    });
+    await msiCreator.create();
+    await msiCreator.compile();
+
+    const msiFile = path.join(root, "Alicorn.msi");
+    const outFile = path.join(root, `Alicorn.${arch}.msi`);
+    await fs.rename(msiFile, outFile);
+
+    consola.success(`MSI installer written to ${outFile}`);
+}
+
+async function buildDMG(outPath: string) {
+    consola.start(`Creating DMG image from ${outPath}`);
+
+    await createDMG({
+        appPath: path.join(outPath, "Alicorn Launcher.app"),
+        name: "Alicorn Launcher",
+        out: outPath + ".dmg"
+    });
+
+    consola.success(`DMG image written to ${outPath + ".dmg"}`);
+}
