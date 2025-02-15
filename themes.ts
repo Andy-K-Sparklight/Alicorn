e9eb02e0aa6c2dc8bbee18d8ca1081d55c5eab9c
skjsjhb <skjsjhb@outlook.com>
2025-02-15 17:46:17 +0800

    fix(ui): fix Tailwind CSS import failure

--- themes.ts
+++ themes.ts
@@ -1,9 +1,12 @@
+// This file is included by Tailwind CSS which does not support TS paths
+// Must use relative path
+
 import type { ConfigThemes } from "@heroui/react";
-import amazingGrace from "~/themes/amazing-grace";
-import overworld from "~/themes/overworld";
-import twikie from "~/themes/twikie";
+import amazingGrace from "./themes/amazing-grace";
+import overworld from "./themes/overworld";
 import sakuraDark from "./themes/sakura-dark";
 import sakuraLight from "./themes/sakura-light";
+import twikie from "./themes/twikie";
 
 export default {
     "sakura-light": sakuraLight,
