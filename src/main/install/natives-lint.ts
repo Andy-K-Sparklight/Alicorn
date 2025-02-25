0b3ad47fefbf63c2952210a5848e70a9a68821ed
skjsjhb <skjsjhb@outlook.com>
2025-02-25 09:27:01 +0800

    perf(install): remove unneeded delay-init of zip library

--- src/main/install/natives-lint.ts
+++ src/main/install/natives-lint.ts
@@ -2,26 +2,15 @@ import type { Container } from "@/main/container/spec";
 import { nativeLib } from "@/main/profile/native-lib";
 import { filterRules } from "@/main/profile/rules";
 import type { VersionProfile } from "@/main/profile/version-profile";
-import { unwrapESM } from "@/main/util/module";
 import fs from "fs-extra";
-import type streamZip from "node-stream-zip";
+import StreamZip from "node-stream-zip";
 import path from "node:path";
 
-let zl: typeof streamZip;
-
-async function getLibrary(): Promise<typeof streamZip> {
-    if (!zl) {
-        zl = (await unwrapESM(import("node-stream-zip"))).default;
-    }
-    return zl;
-}
-
 async function unpackOne(lib: string, out: string, exclude?: string[]): Promise<void> {
-    const z = await getLibrary();
     const filter = (p: string) => !(p.endsWith("/") || exclude?.some(e => p.startsWith(e)));
-    const f = new z.async({ file: lib });
-
+    let f: StreamZip.StreamZipAsync | null = null;
     try {
+        f = new StreamZip.async({ file: lib });
         const files = Object.values(await f.entries()).filter(ent => !ent.isDirectory && filter(ent.name));
 
         await Promise.all(files.map(async ent => {
@@ -29,12 +18,10 @@ async function unpackOne(lib: string, out: string, exclude?: string[]): Promise<
             console.debug(`Extracting native artifact: ${lib} (${ent.name}) -> ${t}`);
 
             await fs.ensureDir(path.dirname(t));
-            await f.extract(ent, t);
+            await f!.extract(ent, t);
         }));
-    } catch (e) {
-        throw e;
     } finally {
-        await f.close();
+        await f?.close();
     }
 }
 
