bc9c5101d4eac5a5cadad7d82c48ef932f272f9f
skjsjhb <skjsjhb@outlook.com>
2025-03-04 18:59:40 +0800

    chore(ui): use array for tip picker

--- src/renderer/components/TipPicker.tsx
+++ src/renderer/components/TipPicker.tsx
@@ -3,8 +3,8 @@ import { useTranslation } from "react-i18next";
 
 export function TipPicker({ tipKey }: { tipKey: string }) {
     const { t } = useTranslation("common", { keyPrefix: "tips" });
-    const values = t(tipKey).split("\n");
-    const [index, setIndex] = useState(Math.floor(Math.random() * values.length));
+    const values = t(tipKey, { returnObjects: true }) as string[];
+    const [index, _] = useState(Math.floor(Math.random() * values.length));
 
     return values[index];
 }
