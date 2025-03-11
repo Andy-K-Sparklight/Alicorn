0f168368328eefdb8b46355787fb2aa5238c025e
skjsjhb <skjsjhb@outlook.com>
2025-03-11 21:28:13 +0800

    feat(ui): add hint for empty items on addons panel

--- src/renderer/components/AddonSearchList.tsx
+++ src/renderer/components/AddonSearchList.tsx
@@ -1,9 +1,9 @@
 import type { MpmAddonMeta, MpmAddonType } from "@/main/mpm/spec";
 import { uniqueBy } from "@/main/util/misc";
 import { AddonMetaDisplay } from "@components/AddonMetaDisplay";
-import { Input, Tab, Tabs } from "@heroui/react";
+import { Input, Spinner, Tab, Tabs } from "@heroui/react";
 import { BlocksIcon, ImagesIcon, SearchIcon, SunIcon } from "lucide-react";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useSessionStorage } from "react-use";
 import { VList, type VListHandle } from "virtua";
@@ -15,9 +15,9 @@ interface AddonSearchListProps {
 export function AddonSearchList({ gameId }: AddonSearchListProps) {
     const { t } = useTranslation("pages", { keyPrefix: "game-detail.manage.addons" });
     const transactionId = useRef(0);
-    const fetching = useRef(false);
+    const [fetching, setFetching] = useState(false);
     const paginationRef = useRef<unknown>(null);
-    const [results, setResults] = useSessionStorage<MpmAddonMeta[]>(`mod-search-results.${gameId}`, []);
+    const [results, setResults] = useSessionStorage<MpmAddonMeta[] | null>(`mod-search-results.${gameId}`, null);
     const vlistRef = useRef<VListHandle | null>(null);
     const searchDelayTimer = useRef<number | null>(null);
 
@@ -47,13 +47,13 @@ export function AddonSearchList({ gameId }: AddonSearchListProps) {
     }
 
     function onScroll() {
-        if (vlistRef.current && vlistRef.current.findEndIndex() + 20 > results.length && !fetching.current) {
+        if (vlistRef.current && results && vlistRef.current.findEndIndex() + 20 > results.length && !fetching) {
             void fetchItems(false);
         }
     }
 
     async function fetchItems(fresh: boolean) {
-        fetching.current = true;
+        setFetching(true);
         transactionId.current++;
         const id = transactionId.current;
 
@@ -62,8 +62,8 @@ export function AddonSearchList({ gameId }: AddonSearchListProps) {
         paginationRef.current = res.pagination;
 
         if (id === transactionId.current) {
-            setResults(fresh ? res.contents : uniqueBy(results.concat(res.contents), r => r.id));
-            fetching.current = false;
+            setResults(fresh ? res.contents : uniqueBy(results!.concat(res.contents), r => r.id));
+            setFetching(false);
         }
     }
 
@@ -91,12 +91,26 @@ export function AddonSearchList({ gameId }: AddonSearchListProps) {
                 }
             </Tabs>
 
-            <Input startContent={<SearchIcon/>} onValueChange={handleQueryChange}/>
+            <Input
+                startContent={<SearchIcon/>}
+                endContent={fetching && <Spinner color="current" size="sm"/>}
+                onValueChange={handleQueryChange}
+            />
         </div>
-        <VList ref={vlistRef} onScroll={onScroll} className="pr-2">
-            {
-                results.map(r => <AddonMetaDisplay key={r.id} gameId={gameId} meta={r}/>)
-            }
-        </VList>
+
+        {
+            (!fetching && results && results.length === 0)
+                ?
+                <div className="grow w-full flex items-center justify-center font-bold text-foreground-400 text-lg">
+                    {t("no-result")}
+                </div>
+                :
+                <VList ref={vlistRef} onScroll={onScroll} className="pr-2 my-auto">
+                    {
+                        results?.map(r => <AddonMetaDisplay key={r.id} gameId={gameId} meta={r}/>)
+                    }
+                </VList>
+        }
+
     </div>;
 }
--- src/renderer/pages/game-detail/LocalAddonsPanel.tsx
+++ src/renderer/pages/game-detail/LocalAddonsPanel.tsx
@@ -1,19 +1,29 @@
 import { useMpmManifest } from "@/renderer/services/mpm";
 import { AddonMetaDisplay } from "@components/AddonMetaDisplay";
 import { useCurrentGameProfile } from "@pages/game-detail/GameProfileProvider";
+import { useTranslation } from "react-i18next";
 import { VList } from "virtua";
 
 export function LocalAddonsPanel() {
     const { id: gameId } = useCurrentGameProfile();
+    const { t } = useTranslation("pages", { keyPrefix: "game-detail.manage.local-addons" });
     const manifest = useMpmManifest(gameId);
 
     if (!manifest) return null;
 
     return <div className="flex flex-col h-full gap-2 px-4">
-        <VList className="pr-2">
-            {
-                manifest.resolved.map(p => <AddonMetaDisplay key={p.meta.id} gameId={gameId} meta={p.meta}/>)
-            }
-        </VList>
+        {
+            manifest.resolved.length === 0 ?
+                <div className="grow flex items-center justify-center font-bold text-foreground-400 text-lg">
+                    {t("empty")}
+                </div>
+                :
+                <VList className="pr-2">
+                    {
+                        manifest.resolved.map(p => <AddonMetaDisplay key={p.meta.id} gameId={gameId} meta={p.meta}/>)
+                    }
+                </VList>
+
+        }
     </div>;
 }
